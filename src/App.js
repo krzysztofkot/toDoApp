e4d74885b70a89ec397cd9f440b9cc13b9daa816
Krzysztof Kot <kootek2@gmail.com>
2021-12-27 22:37:18 +0100

    Added App.js component

--- src/App.js
+++ src/App.js
@@ -4,21 +4,91 @@ import Container from "./UI/Container";
 import TaskForm from "./components/NewTask/TaskForm";
 import ListsContainer from "./components/Lists/ListsContainer";
 
+// const startTasks = [
+//   {
+//     id: 134,
+//     title: "Lorem ipsum dolor sit amet 1",
+//     desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas sint maiores porro labore quae, cumque totam possimus saepe distinctio sequi ea, commodi necessitatibus tempore ipsum ut obcaecati minus enim. Facilis?",
+//   },
+//   {
+//     id: 2112,
+//     title: "Lorem ipsum dolor sit amet consectetur adipisicing elit",
+//     desc: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Reiciendis, harum Lorem ipsum dolor, sit amet consectetur adipisicing elit. Architecto vel natus adipisci?",
+//   },
+// ];
+
+const startTasks = {
+  toDo: [
+    {
+      id: 134,
+      title: "Lorem ipsum dolor sit amet 1",
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas sint maiores porro labore quae, cumque totam possimus saepe distinctio sequi ea, commodi necessitatibus tempore ipsum ut obcaecati minus enim. Facilis?",
+    },
+    {
+      id: 2112,
+      title: "Lorem ipsum dolor sit amet consectetur adipisicing elit",
+      desc: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Reiciendis, harum Lorem ipsum dolor, sit amet consectetur adipisicing elit. Architecto vel natus adipisci?",
+    },
+  ],
+  inProgress: [
+    {
+      id: 134,
+      title: "Lorem ipsum dolor sit amet 1",
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas sint maiores porro labore quae, cumque totam possimus saepe distinctio sequi ea, commodi necessitatibus tempore ipsum ut obcaecati minus enim. Facilis?",
+    },
+    {
+      id: 2112,
+      title: "Lorem ipsum dolor sit amet consectetur adipisicing elit",
+      desc: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Reiciendis, harum Lorem ipsum dolor, sit amet consectetur adipisicing elit. Architecto vel natus adipisci?",
+    },
+  ],
+  done: [
+    {
+      id: 134,
+      title: "Lorem ipsum dolor sit amet 1",
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas sint maiores porro labore quae, cumque totam possimus saepe distinctio sequi ea, commodi necessitatibus tempore ipsum ut obcaecati minus enim. Facilis?",
+    },
+    {
+      id: 2112,
+      title: "Lorem ipsum dolor sit amet consectetur adipisicing elit",
+      desc: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Reiciendis, harum Lorem ipsum dolor, sit amet consectetur adipisicing elit. Architecto vel natus adipisci?",
+    },
+  ],
+};
+const startTasks2 = {
+  toDo: [],
+  inProgress: [],
+  done: [],
+};
+
 function App() {
-  const [tasks, setTasks] = useState([]);
+  const [allTasks, setAllTasks] = useState(startTasks);
 
   const addTaskHandler = task => {
     const id = Date.now().toString().slice(6);
     task.id = id;
-    setTasks(allTasks => {
-      return [...allTasks, task];
+
+    setAllTasks(prevState => {
+      return {
+        ...prevState,
+        toDo: [...prevState.toDo, task],
+      };
     });
   };
 
   let content = <p style={{ textAlign: "center" }}>Please add first goal!</p>;
-
-  if (tasks.length > 0) {
-    content = <ListsContainer allTasks={tasks} />;
+  if (
+    allTasks.toDo.length > 0 ||
+    allTasks.inProgress.length > 0 ||
+    allTasks.done.length > 0
+  ) {
+    content = (
+      <ListsContainer
+        toDoTasks={allTasks.toDo}
+        inProgressTasks={allTasks.inProgress}
+        doneTasks={allTasks.done}
+      />
+    );
   }
 
   return (
